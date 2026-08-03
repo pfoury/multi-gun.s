23d18c454578d7587d74858c75fe0eb53528ed32
pfoury <118745880+pfoury@users.noreply.github.com>
2026-08-03 21:36:02 +0300

    Implement death sound effects

--- scripts/corpse.gd
+++ scripts/corpse.gd
@@ -1,6 +1,15 @@
 extends RigidBody3D
 
+var sound_effect_scene : PackedScene = load("res://scenes/sounds/sound_effect.tscn")
+
 
 func _ready() -> void:
+	# Creating death sound
+	var death_sound_effect = sound_effect_scene.instantiate()
+	
+	death_sound_effect.folder_path = "res://common/sounds/death/"
+	
+	add_child(death_sound_effect)
+	
 	await get_tree().create_timer(2.5).timeout
 	queue_free()
