447973c7b0458e1c2e364e8f273c7e79206a1288
pfoury <118745880+pfoury@users.noreply.github.com>
2026-08-29 00:50:12 +0300

    Implement basic logic for prototype

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,6 +1,9 @@
 extends Node
 
-# Main tab
+# HUD
+@onready var menu: PanelContainer = $MainMenu/Menu
+@onready var close_button: PanelContainer = $MainMenu/CloseButton
+
 @onready var host: Button = $MainMenuOLD/MainTab/VBoxContainer/HBoxContainer/Host
 @onready var host_debug: Button = $MainMenuOLD/MainTab/VBoxContainer/HBoxContainer/HostDebug
 @onready var join: Button = $MainMenuOLD/MainTab/VBoxContainer/HBoxContainer2/Join
@@ -20,13 +23,49 @@ extends Node
 @onready var map_line_edit: LineEdit = $MainMenuOLD/HBoxContainer/VBoxContainer/HBoxContainer2/LineEdit
 
 var args : Dictionary = {}
-
+var is_deploy : bool = false
+var is_customize : bool = false
 
 func _ready() -> void:
 	Global.is_in_game = false
 	
 	camera_player.play("intro")
 	menu_player.play("intro")
+
+
+func _on_deploy_button_pressed() -> void:
+	is_deploy = true
+	menu.hide()
+	close_button.show()
+	camera_player.play("deploy_start")
+
+func _on_close_button_pressed() -> void:
+	if is_deploy:
+		camera_player.play("deploy_end")
+		is_deploy = false
+	elif is_customize:
+		camera_player.play("customization_end")
+		is_customize = false
+	
+	menu.show()
+	close_button.hide()
+
+func _on_customization_button_pressed() -> void:
+	is_customize = true
+	menu.hide()
+	close_button.show()
+	camera_player.play("customization_start")
+
+func _on_settings_button_pressed() -> void:
+	menu.hide()
+	close_button.show()
+
+func _on_about_button_pressed() -> void:
+	menu.hide()
+	close_button.show()
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
 
 
 func creating_args() -> void:
