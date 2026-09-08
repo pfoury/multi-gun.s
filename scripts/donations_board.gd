ac72dc9b8a808c839602a381123d76020ef4a7d3
pfoury <118745880+pfoury@users.noreply.github.com>
2026-09-08 15:12:02 +0300

    Update donators board prototype

--- scripts/donations_board.gd
+++ scripts/donations_board.gd
@@ -4,14 +4,3 @@ const SPEED_DOWN : int = 30
 const SPEED_UP : int = 30
 
 @onready var donators_label: RichTextLabel = $PC/MC/VBC/ScrollContainer/PanelContainer/DonatorsLabel
-@onready var scroll_container: ScrollContainer = $PC/MC/VBC/ScrollContainer
-
-
-func _ready():
-	pass
-
-func _process(delta):
-	if scroll_container != null:
-		scroll_container.scroll_vertical += SPEED_DOWN * delta
-		
-		print(scroll_container.get_v_scroll())
